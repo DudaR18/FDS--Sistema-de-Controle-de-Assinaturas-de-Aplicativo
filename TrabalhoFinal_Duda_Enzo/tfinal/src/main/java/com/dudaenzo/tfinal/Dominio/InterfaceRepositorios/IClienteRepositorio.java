0a5ad92a5e0f5b2e52445c4155ff07461ead2663
Eduarda Rubin <141246640+DudaR18@users.noreply.github.com>
2024-10-20 21:12:01 +0000

    0.5

--- TrabalhoFinal_Duda_Enzo/tfinal/src/main/java/com/dudaenzo/tfinal/Dominio/InterfaceRepositorios/IClienteRepositorio.java
+++ TrabalhoFinal_Duda_Enzo/tfinal/src/main/java/com/dudaenzo/tfinal/Dominio/InterfaceRepositorios/IClienteRepositorio.java
@@ -1,10 +1,19 @@
-package com.dudaenzo.tfinal.Dominio.InterfaceRepositorio;
+package com.dudaenzo.tfinal.Dominio.InterfaceRepositorios;
 
 import java.util.List;
 
-import com.dudaenzo.tfinal.Dominio.entidades.ClienteModel;
+import com.dudaenzo.tfinal.Dominio.Entidade.ClienteModel;
 
 public interface IClienteRepositorio {
-    List<ClienteModel> todos();
-    ClienteModel consultaPorId(long codigo);
+    List<ClienteModel> allClientes();
+
+    ClienteModel consultaPorCodigo(long codigo);
+
+    ClienteModel consultaPorNome(String nome);
+
+    ClienteModel consultaPorEmail(String email);
+
+    ClienteModel salvarCliente(ClienteModel cliente); // cria novo ou atualiza cliente
+
+    void removerCliente(long codigo);
 }
